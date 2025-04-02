991667b48d7da4c563c6efe447f07f3fd561afb9
Ogane-K <syokuensan@gmail.com>
2025-04-03 06:49:35 +0900

    課題018の追加

---
+++ java-basic-kadai/java_kadai/src/kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+ public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+	
+	public KatoHanako_Chapter18() {
+	
+	}
+	
+	
+	
+	//	個別部分の紹介
+	public void eachintroduce() {
+		System.out.println("趣味は読書です");
+	};
+
+}
---
+++ java-basic-kadai/java_kadai/src/kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+ public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+
+	
+	public KatoTaro_Chapter18() {
+	
+	}
+	
+	
+	
+	//	個別部分の紹介
+	public void eachintroduce() {
+		System.out.println("私はJavaが得意です");
+	};
+
+}
